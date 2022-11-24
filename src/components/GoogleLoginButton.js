5e92b604ca75c0350e4af0d225e534b2337a04d3
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2022-11-24 11:44:37 +0100

    google auth with jwt

--- src/components/GoogleLoginButton.js
+++ src/components/GoogleLoginButton.js
@@ -2,38 +2,34 @@ import React from "react";
 import "../sass/css/login.css";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import GoogleIcon from "@mui/icons-material/Google";
-import { GoogleLogin } from "react-google-login";
-import { refreshTokenSetup } from "../utils/refreshTokenSetup";
+// import { GoogleLogin } from "react-google-login";
+// import { refreshTokenSetup } from "../utils/refreshTokenSetup";
+// import { gapi } from "gapi-script";
+import { useEffect } from "react";
 
 const clientId =
 	"224755618921-u8fkf9m7ov2m8a2p6dinnmp549u2gl8a.apps.googleusercontent.com";
 
 function LoginButton() {
-	const onSuccess = res => {
-		console.log("Login udany. Uzytkownik: ", res.profileObj);
+	function handleCallbackResponse(response) {
+		console.log("jwt: ", response.credential);
+	}
 
-		refreshTokenSetup(res);
-	};
+	useEffect(() => {
+		google.accounts.id.initialize({
+			client_id: clientId,
+			callback: handleCallbackResponse,
+		});
 
-	const onFailure = res => {
-		console.log("Login nieudany.", res);
-	};
+		google.accounts.id.renderButton(document.getElementById("googleSignIn"), {
+			theme: "outline",
+			size: "large",
+		});
+	}, []);
+
 	return (
 		<>
-			<div>
-				<GoogleLogin
-					clientId={clientId}
-					render={renderProps => (
-						<button className='icon google-icon' onClick={renderProps.onClick}>
-							<GoogleIcon />
-						</button>
-					)}
-					onSuccess={onSuccess}
-					onFailure={onFailure}
-					cookiePolicy={"single_host_origin"}
-					isSignedIn={true}
-				/>
-			</div>
+			<div id='googleSignIn'></div>
 		</>
 	);
 }

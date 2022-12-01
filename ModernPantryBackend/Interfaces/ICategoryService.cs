ee98cf5aca18bf41a5f6b305bd21c33041acf07e
bw <bartwol111@gmail.com>
2022-12-01 16:06:11 +0100

    added all categories retrieval

---
+++ ModernPantryBackend/Interfaces/ICategoryService.cs
@@ -0,0 +1,7 @@
+﻿namespace ModernPantryBackend.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<ServiceResponse> GetAllCategories();
+    }
+}
---
+++ ModernPantryBackend/Services/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿namespace ModernPantryBackend.Services
+{
+    public class CategoryService : ICategoryService
+    {
+
+        private readonly IBaseRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IBaseRepository<Category> categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ServiceResponse> GetAllCategories()
+        {
+            var categories = await _categoryRepository.FindAll();
+            List<GetCategoryDTO> categoryDTOs = new();
+            foreach(Category category in categories)
+            {
+                categoryDTOs.Add(_mapper.Map<GetCategoryDTO>(category));
+            }
+
+            return ServiceResponse<List<GetCategoryDTO>>.Success(categoryDTOs, "Categories retrieved.");
+        }
+    }
+}

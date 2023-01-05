f9f523cd4d8206aa681693cce28c23478af02a54
bw <bartwol111@gmail.com>
2023-01-05 22:03:54 +0100

    changed notifications return format

--- ModernPantryBackend/Services/NotificationsService.cs
+++ ModernPantryBackend/Services/NotificationsService.cs
@@ -4,31 +4,28 @@
     {
         private readonly IPantryInvitesRepository _pantryInvitesRepository;
         private readonly IHelperService _helperService;
-        private readonly IMapper _mapper;
-        private readonly UserManager<User> _userManager;
         private readonly IPantryRepository _pantryRepository;
 
-        public NotificationsService(IPantryInvitesRepository pantryInvitesRepository, IHelperService helperService, IMapper mapper, UserManager<User> userManager, IPantryRepository pantryRepository)
+        public NotificationsService(IPantryInvitesRepository pantryInvitesRepository, IHelperService helperService,
+            IPantryRepository pantryRepository)
         {
             _pantryInvitesRepository = pantryInvitesRepository;
             _helperService = helperService;
-            _mapper = mapper;
-            _userManager = userManager;
             _pantryRepository = pantryRepository;
         }
 
         public async Task<ServiceResponse> GetUsersNotifications()
         {
-            List<string> notifications = new();
+            List<Notification> notifications = new();
             var currentUser = await _helperService.GetCurrentlyLoggedInUser();
             notifications.AddRange(await GetExpieryNotifications(currentUser));
             notifications.AddRange(await GetInvitesNotifications(currentUser));
-            return ServiceResponse<List<string>>.Success(notifications, "Notifications retrieved.");
+            return ServiceResponse<List<Notification>>.Success(notifications, "Notifications retrieved.");
         }
 
-        private async Task<List<string>> GetExpieryNotifications(User currentUser)
+        private async Task<List<Notification>> GetExpieryNotifications(User currentUser)
         {
-            List<string> expieryNotifications = new();
+            List<Notification> expieryNotifications = new();
             var usersPantries = await _pantryRepository.GetCurrentUserPantries(currentUser.Id);
             foreach(var pantry in usersPantries)
             {
@@ -39,11 +36,11 @@
                         TimeSpan timeSpan = (TimeSpan)(product.ExpieryDate - product.AddDate);
                         if(timeSpan.Days == 0)
                         {
-                            expieryNotifications.Add($"{pantry.Name}: {product.Name} expires today.");
+                            expieryNotifications.Add(new Notification { Information = $"{pantry.Name}: {product.Name} expires today." });
                         }
                         else if(timeSpan.Days <= 3) 
                         {
-                            expieryNotifications.Add($"{pantry.Name}: {product.Name} will expire in {timeSpan.Days} days.");
+                            expieryNotifications.Add(new Notification { Information = $"{pantry.Name}: {product.Name} will expire in {timeSpan.Days} days." });
                         }
                     }
                 }
@@ -51,14 +48,17 @@
             return expieryNotifications;
         }
 
-        private async Task<List<string>> GetInvitesNotifications(User currentUser)
+        private async Task<List<Notification>> GetInvitesNotifications(User currentUser)
         {
-            List<string> inviteNotifications = new();
+            List<Notification> inviteNotifications = new();
             var invites = await _pantryInvitesRepository.FindByConditions(i => i.RecieverId == currentUser.Id);
-            foreach(var invitation in invites)
+            foreach (var invitation in invites)
             {
-                inviteNotifications.Add($"New invite to '{invitation.Pantry.Name}' " +
-                    $"from {invitation.Sender.UserName} ({invitation.Sender.Email})");
+                inviteNotifications.Add(new Notification
+                {
+                    Information = $"New invite to '{invitation.Pantry.Name}' " +
+                    $"from {invitation.Sender.UserName} ({invitation.Sender.Email})"
+                });
             }
             return inviteNotifications;
         }

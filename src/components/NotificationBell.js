9b4ea5fd365b7c9061dc043980d97c1c1ccb4ab9
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2023-01-06 18:36:13 +0100

    viewing notifications in front-end

--- src/components/NotificationBell.js
+++ src/components/NotificationBell.js
@@ -1,80 +1,81 @@
-import React from 'react'
-import Badge from '@mui/material/Badge';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import BasicMenu from './BasicMenu';
+import React from "react";
+import Badge from "@mui/material/Badge";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import BasicMenu from "./BasicMenu";
+import notificationsService from "../services/notifications-service";
 
-const notifications = [
-    {
-        id: 0,
-        label: 'Bread is about to expire'
-    },
-    {
-        id: 1,
-        label: 'You need to resupply on beer'
-    },
-    {
-        id: 2,
-        label: 'Water is about to run out'
-    },
-    {
-        id: 3,
-        label: 'You need to resupply on Cola'
-    },
-    {
-        id: 4,
-        label: 'Bread is about to run out'
-    },
-    {
-        id: 5,
-        label: 'Cheese is about to expire'
-    },
-];
+// const notifications = [
+//     {
+//         label: 'Bread is about to expire'
+//     },
+//     {
+//         label: 'You need to resupply on beer'
+//     },
+//     {
+//         label: 'Water is about to run out'
+//     },
+//     {
+//         label: 'You need to resupply on Cola'
+//     },
+//     {
+//         label: 'Bread is about to run out'
+//     },
+//     {
+//         label: 'Cheese is about to expire'
+//     },
+// ];
 
 const NotificationBell = ({ iconColor }) => {
-    const [open, setOpen] = React.useState(false);
-    const [anchorEl, setAnchorEl] = React.useState(null);
+	const [open, setOpen] = React.useState(false);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [notifications, setNotifications] = React.useState([]);
 
-    const newNotifications = `You have ${notifications.length} new notifications!`;
-    const noNotifications = 'No new notifications';
+	document.addEventListener("DOMContentLoaded", () => {
+		notificationsService.getNotifications().then(response => {
+			setNotifications(response.content);
+		});
+	});
 
-    const handleOpen = (event) => {
-        setAnchorEl(event.currentTarget)
-        setOpen(true);
-    };
+	const newNotifications = `You have ${notifications.length} new notifications!`;
+	const noNotifications = "No new notifications";
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+	const handleOpen = event => {
+		setAnchorEl(event.currentTarget);
+		setOpen(true);
+	};
 
-    return (
-        <div>
-            <Tooltip title={notifications.length ? newNotifications : noNotifications}>
-                <Button 
-                    style={{ color: 'white'}}
-                    color={iconColor}
-                    onClick={notifications.length ? handleOpen : null}
-                    anchorEl={anchorEl}
-                >
-                    <Badge 
-                        style={{marginRight: '5px'}}
-                        badgeContent={notifications.length}
-                        color="error"
-                    >
-                        <NotificationsIcon/>
-                    </Badge>
-                    Notifications
-                </Button>
-            </Tooltip>
-            <BasicMenu
-                open={open}
-                anchorEl={anchorEl}
-                handleClose={handleClose}
-                menuItems={notifications}
-            />
-        </div>
-    )
-}
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-export default NotificationBell
+	return (
+		<div>
+			<Tooltip
+				title={notifications.length ? newNotifications : noNotifications}>
+				<Button
+					style={{ color: "white" }}
+					color={iconColor}
+					onClick={notifications.length ? handleOpen : null}
+					anchorEl={anchorEl}>
+					<Badge
+						style={{ marginRight: "5px" }}
+						badgeContent={notifications.length}
+						color='error'>
+						<NotificationsIcon />
+					</Badge>
+					Notifications
+				</Button>
+			</Tooltip>
+			<BasicMenu
+				open={open}
+				anchorEl={anchorEl}
+				handleClose={handleClose}
+				menuItems={notifications}
+			/>
+		</div>
+	);
+};
+
+export default NotificationBell;

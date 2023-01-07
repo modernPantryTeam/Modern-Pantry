9ca30323b3ef179553318fd605439bdb2806de32
Zayooo00 <konrad6263@o2.pl>
2023-01-07 12:05:45 +0100

    added remaining product services

--- src/services/products-service.js
+++ src/services/products-service.js
@@ -40,6 +40,58 @@ class productsService {
 				return response.data;
 			});
 	}
+
+	async getProductByID(id) {
+
+		return await axios
+			.get(apiUrl + "​/api​/Product​/GetProductById​/" + id, {
+				withCredentials: true,
+				headers: {
+					Authorization: "Bearer " + authService.getToken(),
+				},
+			})
+			.then(response => {
+				return response.data;
+			});
+	}
+
+	async editProduct(name, pantryId, unit, amount, date, category) {
+		var data = JSON.stringify({
+			name: name,
+			pantryId: pantryId,
+			unit: unit,
+			amount: amount,
+			expieryDate: date,
+			categoryIds: category,
+		});
+		return await axios
+		.put(apiUrl + "/api/Product/EditProduct", data, {
+			withCredentials: true,
+			headers: {
+				Authorization: "Bearer " + authService.getToken(),
+				"Content-Type": "application/json",
+			},
+		})
+		.then(response => {
+			return response.data;
+		});
+	}
+
+	async deleteProduct(id) {
+
+		return await axios
+			.delete(apiUrl + "/api/Product/DeleteProduct/" + id, {
+				withCredentials: true,
+				headers: {
+					Authorization: "Bearer " + authService.getToken(),
+				},
+			})
+			.then(response => {
+				window.location.reload();
+				return response.data;
+			});
+	}
 }
+
 
 export default new productsService();

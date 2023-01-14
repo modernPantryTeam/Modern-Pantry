0172c36f21f565284ad9e5069ec43e290ec7b0be
jedrzejowsky <46368935+jedrzejowsky@users.noreply.github.com>
2023-01-14 12:18:51 +0100

    grogu start
    test

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "https://1e05-37-30-108-33.eu.ngrok.io";
+const apiUrl = "https://1e05-37-30-108-33.eu.https://a187-37-30-108-33.eu.ngrok.io.io";
 
 class authService {
 	async login(username, password) {

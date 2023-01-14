c17b9f6d34519fb73be0f282fc3fa71a58197d7b
Zayooo00 <konrad6263@o2.pl>
2023-01-14 13:04:12 +0100

    set defualt rows per page to 10

--- src/pages/Pantry.js
+++ src/pages/Pantry.js
@@ -291,7 +291,7 @@ export default function EnhancedTable() {
 	const [selected, setSelected] = React.useState([]);
 	const [page, setPage] = React.useState(0);
 	const [dense, setDense] = React.useState(false);
-	const [rowsPerPage, setRowsPerPage] = React.useState(5);
+	const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
 	const handleRequestSort = (event, property) => {
 		const isAsc = orderBy === property && order === "asc";
